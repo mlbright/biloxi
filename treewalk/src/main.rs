6292ea2917513ef4fb87ca606ab1471c63fcef22
Martin-Louis Bright <mlbright@gmail.com>
2022-02-19 20:54:45 -0500

    feat: repl

--- treewalk/src/main.rs
+++ treewalk/src/main.rs
@@ -1,5 +1,7 @@
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
@@ -15,12 +17,36 @@ fn main() {
 fn run_file(file: &std::path::Path) {
     let mut file = File::open(file).expect("Unable to open the program file");
     let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Unable to read the program file");
+    file.read_to_string(&mut contents)
+        .expect("Unable to read the program file");
     run(&contents);
 }
 
 fn run_prompt() {
     println!("repl!");
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
 
 fn run(program: &str) {
